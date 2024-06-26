600cd4988863f820d832e7dcaa285a239c850000
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-06-26 16:01:20 +0900

    ExcelDonwloadUtils.java 업데이트

---
+++ src/main/java/kr/cleancode/s891_exceldownload/ExcelDonwloadUtils.java
@@ -0,0 +1,9 @@
+@Component
+@RequiredArgsConstructor
+public final class ExcelDownloadUtils {
+	private static final int MAX_ROW = 10000;
+	private final CommonUtils commonUtils;
+
+	public void getExcelDownload(List<Document> data, String fileName, HttpServletResponse response,)
+
+}
