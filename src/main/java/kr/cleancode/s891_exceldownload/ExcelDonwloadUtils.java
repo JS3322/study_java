f58ffdb5ca1680859e8fa332c3586e089ccfed03
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-06-27 16:09:01 +0900

    ExcelDonwloadUtils.java 업데이트

--- src/main/java/kr/cleancode/s891_exceldownload/ExcelDonwloadUtils.java
+++ src/main/java/kr/cleancode/s891_exceldownload/ExcelDonwloadUtils.java
@@ -31,8 +31,46 @@ public final class ExcelDownloadUtils {
 			workbook.close();
 			outputStream.flush();
 			outputStream.close();
+		}catch (IOEXception | IllegalAccessException e) {
+			throw new RuntimeException(e);
 		}
+	}
 
+//데이터의 값을 추출
+	private List<Object> findFieldValue(Class<?> clazz, Object obj) throws IllegalAccessException {
+		List<Object> result = new ArrayList<>():
+	for (Field field : clazz.getDeclaredFields()) {
+		field.setAccessible(true);
+		result.add(field.get(obj));
+	}
+	return result;
+	}
+
+//엑셀의 헤더 명칭 찾기
+	private List<String> fiendDeaderNames(Class<?> clazz) {
+		return Arrays.strame(clazz.getDeclaredFields()
+			.filter(field -> field.isAnnotationPresent(ExcelColumnName.class))
+			.map(field -> field.getAnnotation(ExcelColumnName.class).deaderName())
+			.collect(Collectors.toList()
+		);
+	}
+
+//
+	private void createBody(Class<?> clazz, List<?> data, Sheet sheet, Row row, Cell cell, int rowNo) throws IllegalAccessException, IOException {
+		int startROw = 0;
+		for (Object o : data) {
+			List<Object> fields = findFieldValue(clazz, o);
+			row = sheet.createRow(++startRow);
+			for(int i = 0; fieldSize = fields.size(); i< fieldSize; i++) {
+				cell = row.createCell(i);
+				cell.setCellValue(commUtils.changeStr(fields.get(i)));
+
+				//주기적인 flush
+				if(rowNo % MAX_ROW==0) {
+					((SXSSFSheet) sheet).flushRows(MAX_ROW);
+				}
+			}
+		}
 	}
 
 }
